eed4ecfce74875db633ffb552377647b6b7c058e
Piotr Sarna <sarna@chiselstrike.com>
2023-03-22 11:48:19 +0100

    Iteration 2: implement separate reader and writer fibers
    The fibers are responsible for sending messages to the server
    and receiving them. The fibers communicate with stream objects
    by channels.
    
    One important TODO left is to remove the busy loop from
    the reader fiber. Instead, it should block until any of the channels
    has available data, and then communicate with the server.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,8 @@
-use hrana_client::Client;
+use hrana_client::{proto::Value, Client};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     // Open a `hrana.Client`, which works like a connection pool in standard SQL
     // databases, but it uses just a single network connection internally
     let url = std::env::var("URL").unwrap_or_else(|_| String::from("localhost:2023"));
@@ -21,6 +22,24 @@ async fn main() {
     for book in books.rows.iter() {
         println!("{:?}", book);
     }
+
+    let steps = stream
+        .execute("EXPLAIN SELECT * FROM book")
+        .await
+        .unwrap()
+        .rows
+        .iter()
+        .map(|step| {
+            if let Some(Value::Text { value: step }) = step.get(1) {
+                step
+            } else {
+                ""
+            }
+            .to_owned()
+        })
+        .collect::<Vec<String>>()
+        .join(", ");
+    println!("Steps: {steps}");
 
     // When you are done, remember to close the client
     client.close().await.unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,118 @@
 use anyhow::{Context, Error, Result};
 use futures::future::FutureExt;
-use handshake::WebSocket;
+use futures::{SinkExt as _, StreamExt as _};
 use proto::Stmt;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio_tungstenite::tungstenite;
 
-mod handshake;
-mod proto;
+pub mod handshake;
+pub mod proto;
 
-pub struct StreamHandle {
-    to_server_receiver: tokio::sync::mpsc::Receiver<proto::ClientMsg>,
-    from_server_sender: tokio::sync::mpsc::Sender<proto::ServerMsg>,
-}
+type ClientReceiver = tokio::sync::mpsc::Receiver<proto::ClientMsg>;
+type ServerSender = tokio::sync::mpsc::Sender<proto::ServerMsg>;
 
 pub struct Client {
-    // FIXME: split to two maps: one for senders, one for receivers
-    stream_handles: Arc<Mutex<HashMap<i32, StreamHandle>>>,
-    _worker_handle: futures::future::RemoteHandle<Result<()>>,
+    client_receiver_handles: Arc<Mutex<HashMap<i32, ClientReceiver>>>,
+    server_sender_handles: Arc<Mutex<HashMap<i32, ServerSender>>>,
+    _reader_handle: futures::future::RemoteHandle<Result<()>>,
+    _writer_handle: futures::future::RemoteHandle<Result<()>>,
 }
 
 impl Client {
     pub async fn open(url: impl Into<String>, jwt: impl Into<String>) -> Result<Self, Error> {
         let url: String = url.into();
         let jwt = jwt.into();
-        println!("Ignored jwt: <{jwt}>");
+        tracing::debug!("Ignored jwt: <{jwt}>");
         let tcp_stream = tokio::net::TcpStream::connect(url.clone()).await?;
-        let mut ws = handshake::handshake_tcp("ws://".to_string() + &url, tcp_stream).await?;
-        println!("Handshake done");
-        Self::send_msg(&mut ws, &proto::ClientMsg::Hello { jwt: None })
-            .await
-            .context("Could not send Hello message")?;
-        Self::recv_msg(&mut ws)
+        let ws = handshake::handshake_tcp("ws://".to_string() + &url, tcp_stream).await?;
+        let (mut write_half, mut read_half) = ws.split();
+        tracing::debug!("Handshake done");
+        write_half
+            .send(Self::serialize_msg(&proto::ClientMsg::Hello { jwt: None })?)
+            .await?;
+        // FIXME: do not receive here, allow pipelining after Hello is sent
+        read_half
+            .next()
             .await
             .context("Receiving HelloOk failed")??;
-        let stream_handles = Arc::new(Mutex::new(HashMap::<i32, StreamHandle>::new()));
-        let stream_handles_for_worker = stream_handles.clone();
-        let (worker_task, _worker_handle) = async move {
-            let mut ws = ws;
-            loop {
-                let mut handles = stream_handles_for_worker.lock().await;
-                for handle in handles.values_mut() {
-                    // FIXME: select! instead of try_recv, or wait on a condition variable until *any* message arrives
-                    if let Ok(msg) = handle.to_server_receiver.try_recv() {
-                        println!("Sending message: {msg:?}");
-                        // Send the message to the server
-                        Self::send_msg(&mut ws, &msg).await?;
-                        let resp = Self::recv_msg(&mut ws)
-                            .await
-                            .ok_or(anyhow::anyhow!("Receiving failed"))??;
-                        println!("Sent message: {resp:?}");
-                        let resp = match resp {
-                            tungstenite::Message::Text(text) => {
-                                serde_json::from_str::<proto::ServerMsg>(&text)
-                                    .context("Could not parse message")
-                            }
+        let client_receiver_handles = Arc::new(Mutex::new(HashMap::<i32, ClientReceiver>::new()));
+        let server_sender_handles = Arc::new(Mutex::new(HashMap::<i32, ServerSender>::new()));
+        let client_receiver_handles_ = client_receiver_handles.clone();
+        let server_sender_handles_ = server_sender_handles.clone();
+        let request_id_to_stream_for_reader =
+            Arc::new(std::sync::Mutex::new(HashMap::<i32, i32>::new()));
+        let request_id_to_stream_for_writer = request_id_to_stream_for_reader.clone();
 
-                            _ => Err(anyhow::anyhow!("Unexpected message")),
-                        }?;
-                        handle.from_server_sender.send(resp).await?;
+        // The reader fiber
+        let (reader_task, _reader_handle) = async move {
+            let mut write_half = write_half;
+            loop {
+                // The lock is expected not to be contended, except for open_stream(), which is supposed to be rare
+                let mut receiver_handles = client_receiver_handles_.lock().await;
+                for (stream_id, receiver_handle) in receiver_handles.iter_mut() {
+                    // FIXME: select! instead of try_recv, or wait on a condition variable until *any* message arrives
+                    if let Ok(msg) = receiver_handle.try_recv() {
+                        tracing::debug!("Sending message: {msg:?}");
+                        let request_id = if let proto::ClientMsg::Request { request_id, .. } = &msg
+                        {
+                            request_id
+                        } else {
+                            anyhow::bail!("Unexpected message: RequestMsg expected")
+                        };
+                        tracing::debug!("Sending message from stream {stream_id}: {request_id}");
+                        {
+                            let mut request_id_to_stream =
+                                request_id_to_stream_for_reader.lock().unwrap();
+                            request_id_to_stream.insert(*request_id, *stream_id);
+                        }
+                        // Send the message to the server
+                        write_half.send(Self::serialize_msg(&msg)?).await?;
                     }
                 }
             }
         }
         .remote_handle();
-        tokio::spawn(worker_task);
+        tokio::spawn(reader_task);
+
+        // The writer fiber
+        let (writer_task, _writer_handle) = async move {
+            let mut read_half = read_half;
+            loop {
+                let resp = read_half.next().await.context("Receiving failed")??;
+                let resp = match resp {
+                    tungstenite::Message::Text(text) => {
+                        serde_json::from_str::<proto::ServerMsg>(&text)
+                            .context("Could not parse message")
+                    }
+
+                    _ => Err(anyhow::anyhow!("Unexpected message")),
+                }?;
+                tracing::debug!("Received message: {resp:?}");
+                let mut sender_handles = server_sender_handles_.lock().await;
+                let request_id = match &resp {
+                    proto::ServerMsg::ResponseOk { request_id, .. } => request_id,
+                    proto::ServerMsg::ResponseError { request_id, .. } => request_id,
+                    _ => anyhow::bail!("Unexpected message: ResponseMsg expected"),
+                };
+                let stream_id = {
+                    let mut request_id_to_stream = request_id_to_stream_for_writer.lock().unwrap();
+                    request_id_to_stream.remove(request_id).unwrap()
+                };
+                tracing::debug!("Sending message to stream {stream_id}: {request_id}");
+                let sender_handle = sender_handles.get_mut(&stream_id).unwrap();
+                sender_handle.send(resp).await?;
+            }
+        }
+        .remote_handle();
+        tokio::spawn(writer_task);
+
         let client = Self {
-            stream_handles,
-            _worker_handle,
+            client_receiver_handles,
+            server_sender_handles,
+            _reader_handle,
+            _writer_handle,
         };
         Ok(client)
     }
@@ -78,44 +122,36 @@ impl Client {
         let (from_server_sender, mut from_server_receiver) = tokio::sync::mpsc::channel(1024);
 
         // FIXME: if we assume that open_stream is rare, we can use an RCU-like scheme
-        let mut handles = self.stream_handles.lock().await;
-        let stream_id = handles.len() as i32;
-        handles.insert(
-            stream_id,
-            StreamHandle {
-                to_server_receiver,
-                from_server_sender,
-            },
-        );
-        drop(handles);
+        let stream_id = {
+            let mut client_receiver_handles = self.client_receiver_handles.lock().await;
+            let mut server_sender_handles = self.server_sender_handles.lock().await;
+            let stream_id = client_receiver_handles.len() as i32;
+            client_receiver_handles.insert(stream_id, to_server_receiver);
+            server_sender_handles.insert(stream_id, from_server_sender);
+            stream_id
+        };
 
         let req = proto::ClientMsg::Request {
             request_id: 0,
-            request: proto::Request::OpenStream(proto::OpenStreamReq { stream_id: 0 }),
+            request: proto::Request::OpenStream(proto::OpenStreamReq { stream_id }),
         };
         to_server_sender.send(req).await?;
+        // FIXME: do not receive here, allow pipelining after OpenStream is sent
         let resp = from_server_receiver
             .recv()
             .await
             .context("Receiving failed")?;
-        println!("Response: {resp:?}");
-        Ok(Stream {
-            id: 0,
+        tracing::debug!("Response: {resp:?}");
+        Ok(Stream::new(
+            stream_id,
             to_server_sender,
             from_server_receiver,
-        })
+        ))
     }
 
-    pub async fn send_msg(ws: &mut WebSocket, msg: &proto::ClientMsg) -> Result<()> {
+    fn serialize_msg(msg: &proto::ClientMsg) -> Result<tungstenite::Message> {
         let msg = serde_json::to_string(&msg).context("Could not serialize response message")?;
-        let msg = tungstenite::Message::Text(msg);
-        ws.send(msg)
-            .await
-            .context("Could not send response to the WebSocket")
-    }
-
-    pub async fn recv_msg(ws: &mut WebSocket) -> Option<tungstenite::Result<tungstenite::Message>> {
-        ws.recv().await
+        Ok(tungstenite::Message::Text(msg))
     }
 
     pub async fn close(&mut self) -> Result<()> {
@@ -128,15 +164,35 @@ impl Client {
 // that runs I/O operations in a separate fiber and communicates via channels.
 pub struct Stream {
     id: i32,
+    next_request_id: i32,
     to_server_sender: tokio::sync::mpsc::Sender<proto::ClientMsg>,
     from_server_receiver: tokio::sync::mpsc::Receiver<proto::ServerMsg>,
 }
 
 impl Stream {
+    fn new(
+        stream_id: i32,
+        to_server_sender: tokio::sync::mpsc::Sender<proto::ClientMsg>,
+        from_server_receiver: tokio::sync::mpsc::Receiver<proto::ServerMsg>,
+    ) -> Self {
+        Self {
+            id: stream_id,
+            next_request_id: 1,
+            to_server_sender,
+            from_server_receiver,
+        }
+    }
+
+    fn next_request_id(&mut self) -> i32 {
+        let request_id = self.next_request_id;
+        self.next_request_id += 1;
+        request_id
+    }
+
     pub async fn execute(&mut self, stmt: impl Into<String>) -> Result<proto::StmtResult> {
         let stmt = stmt.into();
         let req = proto::ClientMsg::Request {
-            request_id: 0,
+            request_id: self.next_request_id(),
             request: proto::Request::Execute(proto::ExecuteReq {
                 stream_id: self.id,
                 stmt: Stmt {
